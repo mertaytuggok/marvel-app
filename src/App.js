e8f8fc4df2eae8e1fa8dc8070ff00be8bf29c813
mertaytuggok <mertaytuggok@gmail.com>
2023-01-09 00:21:53 +0300

    Router structure added.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Header } from "./Components/Header/Header";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 import { Dashboard } from "./Components/Dashboard/Dashboard";
+import { LandingPage } from "./Pages/LandingPage/LandingPage";
+import { DetailCard } from "./Components/DetailCard/DetailCard";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Dashboard />
+      {" "}
+      <BrowserRouter>
+        <Routes>
+          {" "}
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/" element={<LandingPage />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="/char/:id" element={<DetailCard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
