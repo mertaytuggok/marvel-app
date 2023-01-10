f31cd95a536bcbd88536ee5d0f36266e003d5e53
mertaytuggok <mertaytuggok@gmail.com>
2023-01-10 19:59:34 +0300

    Router structure fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./Components/Dashboard/Dashboard";
 import { LandingPage } from "./Pages/LandingPage/LandingPage";
 import { DetailCard } from "./Components/DetailCard/DetailCard";
+import { Comics } from "./Components/Comics/Comics";
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           {" "}
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/" element={<Navigate to="/characters" replace />} />
           <Route path="/" element={<LandingPage />}>
-            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="characters" element={<Dashboard />} />
             <Route path="/char/:id" element={<DetailCard />} />
+            <Route path="comics" element={<Comics />} />
           </Route>
         </Routes>
       </BrowserRouter>
