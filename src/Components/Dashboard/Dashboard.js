e67186c8695ca68ca4dd5a133e15febe75780c3b
mertaytuggok <mertaytuggok@gmail.com>
2023-01-10 20:00:54 +0300

    Dashboard design has been changed.

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,46 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchCharaters } from "../../ReduxSlice/characterSlice";
+import { fetchCharaters } from "../../ReduxSlice/marvelSlice";
 import styles from "./Dashboard.module.scss";
 
 export const Dashboard = () => {
-  const characters = useSelector((state) => state.characters.character.data);
-  const isLoading = useSelector((state) => state.characters.isLoading);
-  const error = useSelector((state) => state.characters.error);
+  const characters = useSelector((state) => state.marvel.character.data);
+  const error = useSelector((state) => state.marvel.error);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCharaters());
   }, []);
-  if (isLoading) {
-    return <div>Loading..</div>;
-  }
+
   if (error) {
     return <div>Error: {error}</div>;
   }
   return (
-    <div className={styles.mainDashboard}>
-      <div className={styles.infoCard}>
-        {characters?.results.map((item) => (
-          <Link
-            key={item.id}
-            className={styles.linkText}
-            to={`/char/${item.id}`}
-          >
-            <div className={styles.card}>
+    <>
+      <div className={styles.mainDashboard}>
+        <div className={styles.infoCard}>
+          {characters?.results.map((item) => (
+            <Link
+              key={item.id}
+              className={styles.linkText}
+              to={`/char/${item.id}`}
+            >
               <img
                 className={styles.image}
                 alt={item.name}
                 src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
               />
-
-              <div>{item.name}</div>
-              <div>Desc</div>
-              <div>Button</div>
-            </div>
-          </Link>
-        ))}{" "}
+              <div className={styles.cardText}>
+                <span>{item.name}</span>
+                <span className={styles.grayText}>
+                  {item.comics.available} Comics
+                </span>
+              </div>
+            </Link>
+          ))}{" "}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
