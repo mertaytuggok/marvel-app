aac1aa74aa79c6405d8959a37c31658f2d5585ff
mertaytuggok <mertaytuggok@gmail.com>
2023-01-10 20:01:08 +0300

    Header design has been changed.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
+import Logo from "../../Asset/Logo.svg";
 
 export const Header = () => {
   return (
-    <div className={styles.mainHeadder}>
+    <>
       {" "}
-      <NavLink className={styles.headerText} to="dashboard">
-        Ana Sayfa
+      <NavLink className={styles.topHeader} to="characters">
+        {" "}
+        <img src={Logo} />
       </NavLink>
-      <NavLink className={styles.headerText} to="detail">
-        Detail
-      </NavLink>
-    </div>
+      <div className={styles.bottomHeader}>
+        <NavLink className={styles.headerText} to="characters">
+          CHARACTERS
+        </NavLink>
+        <NavLink className={styles.headerText} to="comics">
+          COMİCS
+        </NavLink>
+      </div>
+    </>
   );
 };
